17e9064e1dcf8622dd7691c1478d3f595cd372e1
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-14 16:30:14 -0500

    Init calendar try 1

---
+++ calendar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
+
+//Goal: Print out one full year calendar in the terminal
+//12 months, starting at January, to December
+//Simple calendar format, Sun Mon Tue Wed Thu Fri Sat or so
+//Year will be chosen by the user, so it must make sense dynamically
+//No idea how a calendar is calculated
+
