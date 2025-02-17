8ef9f4888544b604a494bfdd54549efaead84a9d
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-17 14:55:39 -0500

    CPL Exercise 1-7 print EOF

---
+++ print_eof_value.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+//The C Programming Language - Exercise 1-7
+//Write a program to print the value of EOF.
+int main(void)
+{
+	int end_of_file = EOF;
+	printf("%s%d\n", "Print the value of end-of-file: ", end_of_file);
+	return 0;
+}
