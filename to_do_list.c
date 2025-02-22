f9887821d135f42a189896bbfe0c96679f41d3f4
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-21 19:23:09 -0500

    Add numbered or bullet list option

--- to_do_list.c
+++ to_do_list.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void create_task_string(int task_count);
+void create_task_string(int task_count, int is_style_numbered);
 
 //No clue how to set that up just in the command line without any GUI
 //So let's just create the general functionality and print it to the console
@@ -8,31 +8,29 @@ void create_task_string(int task_count);
 //So I suppose we need to first be capable of string inputs that will be the task string name
 //"Clean your room" for example, print that input string with all other to-do-list tasks
 //We can also use a mode in which it's either a bullet list, or, a numbered list
-//Plus we need to be capable of deactivating/deleting to-do-list tasks after they're done
-//And if you just type enter the current list of tasks should be printed
 int main(void)
 {
-	/*printf("%s\n", "Type B for bullet list style, and N for numbered list style: ");
+	printf("%s\n", "Type B for bullet list style, and N for numbered list style: ");
 	char style = ' ';
 	scanf("%c", &style);
+	int is_style_numbered = 0;
 	if(style == 'b' || style == 'B')
 	{
-
+		is_style_numbered = 0;
 	}
 	else if(style == 'n' || style == 'N')
 	{
-
+		is_style_numbered = 1;
 	}
 	else
 	{
 		printf("%s\n", "Wrong input, try again");
 	}
-	*/
 	printf("%s\n", "This is your to-do-list, just enter a task to add it to your task list: ");
 	const int TASK_AMOUNT_LIMIT = 10 + 1; 
 	for(int i = 1; i < TASK_AMOUNT_LIMIT; ++i)
 	{
-		create_task_string(i);
+		create_task_string(i, is_style_numbered);
 	}
 	return 0;
 }
@@ -41,7 +39,7 @@ int main(void)
 //Every time I run the script the behavior changes, as if the last test
 //Is influencing the current test from a different runtime
 //Will fix it (hopefully) after learning how arrays and printf works
-void create_task_string(int task_count)
+void create_task_string(int task_count, int is_style_numbered)
 {
 	int task_character_count = 0;
 	char current_character = ' ';
@@ -69,20 +67,18 @@ void create_task_string(int task_count)
 	}
 	for(int i = 0; i < task_count; ++i)
 	{
+		if(is_style_numbered == 1)
+		{
+			printf("%d ", task_count - i - 1);
+		}
+		else if(is_style_numbered == 0)
+		{
+			printf("%c ", 'o');
+		}
 		printf("%s\n", tasks[task_count - i]);
 	}
 	printf("\n");
 	//printf("%s\n", task_string);
 	//printf("%s\n", tasks[task_list_amount]);
 	//printf("%d\n", task_character_count);
-}
-
-void print_numbered_task_list()
-{
-
-}
-
-void print_bullet_task_list()
-{
-
 }
